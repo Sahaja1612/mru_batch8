671d44c54de829468324fd8f2595114ddee446e7
Sahaja1612 <2311cs010556@mallareddyuniversity.ac.in>
2025-01-11 11:09:49 +0530

    oops3

---
+++ src/com/mru/oops/oops4.java
@@ -0,0 +1,17 @@
+package com.mru.oops;
+public class oops4 {	
+	public void addition(int num1, int num2) {
+		int res1 = num1 + num2;
+		System.out.println(res1);
+		}	
+	public void addition(int num1, int num2, int num3) {
+		int res2 = num1 + num2 + num3;
+		System.out.println(res2);
+		}
+	public static void main(String[] args) {
+		oops4 obj = new oops4();
+		obj.addition(10, 5);
+		obj.addition(10, 5, 3);
+	}
+
+}
