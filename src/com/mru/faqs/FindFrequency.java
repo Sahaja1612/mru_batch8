bbd70c3cea18205adbedc87627ad2ccafbc52492
Sahaja1612 <2311cs010556@mallareddyuniversity.ac.in>
2025-01-28 09:40:54 +0530

    Assignments1

---
+++ src/com/mru/faqs/FindFrequency.java
@@ -0,0 +1,18 @@
+package com.mru.faqs;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindFrequency {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 2, 3, 4, 2, 3, 4, 3, 4, 4};
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int element : arr) {
+            map.put(element, map.getOrDefault(element, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + "frequency" + entry.getValue());
+        }
+    }
+}
