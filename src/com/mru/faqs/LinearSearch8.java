dbbe1c9cb4d842c53fd404fa5a634664af450b6d
Sahaja1612 <2311cs010556@mallareddyuniversity.ac.in>
2025-01-30 11:12:08 +0530

    Searching1

---
+++ src/com/mru/faqs/LinearSearch8.java
@@ -0,0 +1,22 @@
+package com.mru.faqs;
+public class LinearSearch8 {
+	public static int linear_search(int[] arr, int search_element) {
+		for(int i=0;i<=arr.length;i++) {
+			if(arr[i] == search_element) {
+				return i;
+			}
+		}
+		return -1;
+	}
+		
+	public static void main(String[] args) {
+		int res =  linear_search(new int[] {10,20,30,40,50}, 30);
+		if(res != -1) 
+			System.out.println("Element Found At Index Postition :"+res);
+			else {
+				System.out.println("Element Not Found !!!");
+			}
+		}
+	}
+
+
